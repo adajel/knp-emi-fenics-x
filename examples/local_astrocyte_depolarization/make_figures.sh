ce0c22efd040f74c88ca5738a6b3308802358428
adajel <ada.joahanne.ellingsrud@gmail.com>
2026-05-20 13:59:41 +0200

    remove file

--- examples/local_astrocyte_depolarization/make_figures.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 make_figures.py -c ICS-tort-x10
-python3 make_figures.py -c ICS-tort-x20
-python3 make_figures.py -c ECS-tort-x10
-python3 make_figures.py -c ECS-tort-x20
-python3 make_figures.py -c baseline
